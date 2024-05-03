a64006380f9944b3b51eb5a0598c37b30cde8803
Mateus Pfeffer <mateuspfeffer@gmail.com>
2024-05-02 21:38:33 -0300

    Database connection and http server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,78 @@
-fn main() {
-    println!("Hello, world!");
+mod config;
+mod db;
+
+use actix_cors::Cors;
+use actix_web::{http::header, middleware::Logger, web, App, HttpServer};
+use config::Config;
+use db::DBClient;
+use dotenv::dotenv;
+use sqlx::postgres::PgPoolOptions;
+
+#[derive(Debug, Clone)]
+pub struct AppState {
+    pub env: Config,
+    pub db_client: DBClient,
+}
+
+#[actix_web::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "actix_web=info");
+    }
+
+    dotenv().ok();
+    // env_logger::init();
+
+    let config = Config::init();
+
+    println!("{}", &config.database_url);
+
+    let pool = PgPoolOptions::new()
+        .max_connections(10)
+        .connect(&config.database_url)
+        .await?;
+
+    println!("AQUI");
+
+    // match sqlx::migrate!("./migrations").run(&pool).await {
+    //     Ok(_) => println!("Migrations executed successfully."),
+    //     Err(e) => eprintln!("Error executing migrations: {}", e),
+    // };
+
+    let db_client = DBClient::new(pool);
+    let app_state: AppState = AppState {
+        env: config.clone(),
+        db_client,
+    };
+
+    println!(
+        "{}",
+        format!("Server is running on http://localhost:{}", config.port)
+    );
+
+    HttpServer::new(move || {
+        let cors = Cors::default()
+            .allowed_origin("http://localhost:3000")
+            .allowed_origin("http://localhost:8000")
+            .allowed_methods(vec!["GET", "POST"])
+            .allowed_headers(vec![
+                header::CONTENT_TYPE,
+                header::AUTHORIZATION,
+                header::ACCEPT,
+            ])
+            .supports_credentials();
+
+        App::new()
+            .app_data(web::Data::new(app_state.clone()))
+            .wrap(cors)
+            .wrap(Logger::default())
+            // .service(scopes::auth::auth_scope())
+            // .service(scopes::users::users_scope())
+            // .service(health_checker_handler)
+    })
+    .bind(("0.0.0.0", config.port))?
+    .run()
+    .await?;
+
+    Ok(())
 }
