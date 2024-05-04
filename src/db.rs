d28e3c6b245b627610384a0f5005f1f90fa219c3
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-05-04 14:58:23 -0300

    CountryExt trait and implementation
    Routes to get, list and create a country

--- src/db.rs
+++ src/db.rs
@@ -1,4 +1,8 @@
+use async_trait::async_trait;
 use sqlx::{Pool, Postgres};
+use uuid::Uuid;
+
+use crate::models::Country;
 
 #[derive(Debug, Clone)]
 pub struct DBClient {
@@ -8,5 +12,106 @@ pub struct DBClient {
 impl DBClient {
     pub fn new(pool: Pool<Postgres>) -> Self {
         DBClient { pool }
+    }
+}
+
+#[async_trait]
+pub trait CountryExt {
+    async fn get_country(
+        &self,
+        country_id: Option<Uuid>,
+        name: Option<&str>,
+        alpha_2: Option<&str>,
+        alpha_3: Option<&str>,
+        numeric_3: Option<&str>,
+        capital: Option<&str>,
+    ) -> Result<Option<Country>, sqlx::Error>;
+
+    async fn list_countries(&self, page: u32, limit: usize) -> Result<Vec<Country>, sqlx::Error>;
+
+    async fn save_country<T: Into<String> + Send>(
+        &self,
+        name: T,
+        alpha_2: T,
+        alpha_3: T,
+        numeric_3: T,
+        capital: T,
+    ) -> Result<Country, sqlx::Error>;
+}
+
+#[async_trait]
+impl CountryExt for DBClient {
+    async fn get_country(
+        &self,
+        country_id: Option<uuid::Uuid>,
+        name: Option<&str>,
+        alpha_2: Option<&str>,
+        alpha_3: Option<&str>,
+        numeric_3: Option<&str>,
+        capital: Option<&str>,
+    ) -> Result<Option<Country>, sqlx::Error> {
+        let mut country: Option<Country> = None;
+
+        if let Some(country_id) = country_id {
+            country = sqlx::query_as!(Country, r#"SELECT id, name, alpha_2, alpha_3, numeric_3, capital FROM countries WHERE id = $1"#, country_id)
+                .fetch_optional(&self.pool)
+                .await?;
+        } else if let Some(name) = name {
+            country = sqlx::query_as!(Country, r#"SELECT id, name, alpha_2, alpha_3, numeric_3, capital FROM countries WHERE name = $1"#, name)
+                .fetch_optional(&self.pool)
+                .await?;
+        } else if let Some(alpha_2) = alpha_2 {
+            country = sqlx::query_as!(Country, r#"SELECT id, name, alpha_2, alpha_3, numeric_3, capital FROM countries WHERE alpha_2 = $1"#, alpha_2) 
+                .fetch_optional(&self.pool)
+                .await?;
+        } else if let Some(alpha_3) = alpha_3 {
+            country = sqlx::query_as!(Country, r#"SELECT id, name, alpha_2, alpha_3, numeric_3, capital FROM countries WHERE alpha_3 = $1"#, alpha_3) 
+                .fetch_optional(&self.pool)
+                .await?;
+        } else if let Some(numeric_3) = numeric_3 {
+            country = sqlx::query_as!(Country, r#"SELECT id, name, alpha_2, alpha_3, numeric_3, capital FROM countries WHERE numeric_3 = $1"#, numeric_3) 
+                .fetch_optional(&self.pool)
+                .await?;
+        }
+
+        Ok(country)
+    }
+
+    async fn list_countries(&self, page: u32, limit: usize) -> Result<Vec<Country>, sqlx::Error> {
+        let offset = (page - 1) * limit as u32;
+
+        let countries = sqlx::query_as!(
+            Country,
+            r#"SELECT id, name, alpha_2, alpha_3, numeric_3, capital FROM countries LIMIT $1 OFFSET $2"#,
+            limit as i64,
+            offset as i64
+        )
+        .fetch_all(&self.pool)
+        .await?;
+
+        Ok(countries)
+    }
+
+    async fn save_country<T: Into<String> + Send>(
+        &self,
+        name: T,
+        alpha_2: T,
+        alpha_3: T,
+        numeric_3: T,
+        capital: T,
+    ) -> Result<Country, sqlx::Error> {
+        let country = sqlx::query_as!(
+            Country,
+            r#"INSERT INTO countries (name, alpha_2, alpha_3, numeric_3, capital) VALUES ($1, $2, $3, $4, $5) RETURNING id, name, alpha_2, alpha_3, numeric_3, capital"#,
+            name.into(),
+            alpha_2.into(),
+            alpha_3.into(),
+            numeric_3.into(),
+            capital.into()
+        )
+        .fetch_one(&self.pool)
+        .await?;
+
+        Ok(country)
     }
 }
