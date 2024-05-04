faae1746c4f8023dd15d0a06ecf4339e05abcad3
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-05-04 14:57:37 -0300

    Created the models file.
    Added Country model

---
+++ src/models.rs
@@ -0,0 +1,11 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, sqlx::FromRow, sqlx::Type, Serialize, Clone)]
+pub struct Country {
+    pub id: uuid::Uuid,
+    pub name: String,
+    pub alpha_2: String,
+    pub alpha_3: String,
+    pub numeric_3: String,
+    pub capital: String,
+}
