39aa68e649b909e72b1d0c7bcba5e93123d28cc0
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-05-04 17:45:00 -0300

    Route to delete a country by id

--- src/scopes/countries.rs
+++ src/scopes/countries.rs
@@ -13,6 +13,7 @@ pub fn countries_scope() -> Scope {
         .route("", web::get().to(list_countries))
         .route("/{id}", web::get().to(get_country))
         .route("", web::post().to(save_country))
+        .route("/{id}", web::delete().to(delete_country))
 }
 
 pub async fn get_country(
@@ -78,4 +79,17 @@ pub async fn save_country(
         }
         Err(e) => Err(HttpError::server_error(e.to_string())),
     }
+}
+
+pub async fn delete_country(
+    id: web::Path<uuid::Uuid>,
+    app_state: web::Data<AppState>,
+) -> Result<HttpResponse, HttpError> {
+    let country = app_state
+        .db_client
+        .delete_country(Some(id.into_inner()))
+        .await
+        .map_err(|e| HttpError::server_error(e.to_string()))?;
+
+    Ok(HttpResponse::Ok().json(FilterCountryDTO::filter_country(&country.unwrap())))
 }
