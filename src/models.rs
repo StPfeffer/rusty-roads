96d953f2f6bbb04d15fd40e494a3aecbedbf8dc6
Mateus Pfeffer <mateuspfeffer@gmail.com>
2024-05-10 14:34:10 -0300

    Added State model

--- src/models.rs
+++ src/models.rs
@@ -1,10 +1,19 @@
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
 #[derive(Debug, Deserialize, sqlx::FromRow, sqlx::Type, Serialize, Clone)]
 pub struct Country {
-    pub id: uuid::Uuid,
+    pub id: Uuid,
     pub name: String,
     pub alpha_2: String,
     pub alpha_3: String,
     pub numeric_3: String,
+}
+
+#[derive(Debug, Deserialize, sqlx::FromRow, sqlx::Type, Serialize, Clone)]
+pub struct State {
+    pub id: Uuid,
+    pub name: String,
+    pub code: String,
+    pub country_id: Uuid,
 }
