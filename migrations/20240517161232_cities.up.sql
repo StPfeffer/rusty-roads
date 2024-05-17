eec3895b237be6734a78a2cf0058df132c1f9738
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-05-17 14:52:59 -0300

    Updated the cities migration

--- migrations/20240517161232_cities.up.sql
+++ migrations/20240517161232_cities.up.sql
@@ -1,8 +1,8 @@
 -- Add up migration script here
-CREATE TABLE CITIES
+CREATE TABLE cities
 (
     id         UUID         NOT NULL PRIMARY KEY DEFAULT (uuid_generate_v4()),
-    name       VARCHAR(100) NOT NULL UNIQUE,
+    name       VARCHAR(100) NOT NULL,
     code       VARCHAR(7)   NOT NULL UNIQUE,
     state_id   UUID         NOT NULL
         CONSTRAINT fk_cities_state_id
