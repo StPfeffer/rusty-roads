24c2d0adcfce57bab1f33ea08a72c1316a4110d1
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-05-17 13:20:40 -0300

    Created City DTOs

---
+++ src/dtos/city.rs
@@ -0,0 +1,56 @@
+use serde::{Deserialize, Serialize};
+use validator::Validate;
+
+use crate::models::city::City;
+
+#[derive(Validate, Debug, Default, Clone, Serialize, Deserialize)]
+pub struct RegisterCityDTO {
+    #[validate(length(
+        min = 1,
+        max = 100,
+        message = "City name must have a maximum of 100 characters"
+    ))]
+    pub name: String,
+
+    #[validate(length(min = 2, max = 2, message = "City code must be 2 characters long."))]
+    pub code: String,
+
+    #[serde(rename = "stateId")]
+    pub state_id: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct FilterCityDTO {
+    pub id: String,
+    pub name: String,
+    pub code: String,
+    #[serde(rename = "stateId")]
+    pub state_id: String,
+}
+
+impl FilterCityDTO {
+    pub fn filter_city(city: &City) -> Self {
+        FilterCityDTO {
+            id: city.id.to_string(),
+            name: city.name.to_owned(),
+            code: city.code.to_owned(),
+            state_id: city.state_id.to_string(),
+        }
+    }
+
+    pub fn filter_cities(cities: &[City]) -> Vec<FilterCityDTO> {
+        cities.iter().map(FilterCityDTO::filter_city).collect()
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct CityResponseDTO {
+    pub status: String,
+    pub data: FilterCityDTO,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct CityListResponseDTO {
+    pub cities: Vec<FilterCityDTO>,
+    pub results: usize,
+}
