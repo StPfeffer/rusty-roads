28842b75c9185b8378afb84e7acb36b119bd8fa8
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-05-19 21:15:29 -0300

    Created Vehicle model

---
+++ src/models/vehicle.rs
@@ -0,0 +1,12 @@
+use chrono::NaiveDateTime;
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+#[derive(Debug, Deserialize, sqlx::Type, Serialize, Clone)]
+pub struct Vehicle {
+    pub id: Uuid,
+    pub initial_mileage: i32,
+    pub actual_mileage: i32,
+    pub created_at: NaiveDateTime,
+    pub updated_at: NaiveDateTime,
+}
