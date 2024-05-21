ad0e69611cca202da17e5accd85163b214b2e868
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-05-20 22:02:16 -0300

    Added collaborator up and down migrations

---
+++ migrations/20240521004843_collaborator.up.sql
@@ -0,0 +1,11 @@
+-- Add up migration script here
+CREATE TABLE collaborators
+(
+    id         UUID                    NOT NULL PRIMARY KEY DEFAULT (uuid_generate_v4()),
+    name       VARCHAR(100)            NOT NULL,
+    email      VARCHAR(150) UNIQUE     NOT NULL,
+    cpf        VARCHAR(11)  UNIQUE     NOT NULL,
+    rg         VARCHAR(9)              NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW() NOT NULL,
+    updated_at TIMESTAMP DEFAULT NOW() NOT NULL
+);
