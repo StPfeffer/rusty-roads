54d0c367915187cb124dd1232be25dd4791d6e82
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-05-23 14:52:43 -0300

    Added name column to vehicle migration

--- migrations/20240519232925_vehicles.up.sql
+++ migrations/20240519232925_vehicles.up.sql
@@ -2,6 +2,7 @@
 CREATE TABLE vehicles
 (
     id              UUID                    NOT NULL PRIMARY KEY DEFAULT (uuid_generate_v4()),
+    name            VARCHAR(60)             NOT NULL,
     initial_mileage INTEGER                 NOT NULL,
     actual_mileage  INTEGER                 NOT NULL,
     created_at      TIMESTAMP DEFAULT NOW() NOT NULL,
