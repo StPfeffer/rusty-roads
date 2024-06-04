debda802e09a8d6ffad164181d82fe472300bf2c
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-06-03 22:19:19 -0300

    Created Route model

---
+++ src/models/route.rs
@@ -0,0 +1,23 @@
+use bigdecimal::BigDecimal;
+use chrono::NaiveDateTime;
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+#[derive(Debug, Deserialize, sqlx::FromRow, Serialize, Clone)]
+pub struct Route {
+    pub id: Uuid,
+    pub started_at: NaiveDateTime,
+    pub ended_at: Option<NaiveDateTime>,
+    pub total_distance: BigDecimal,
+    pub created_at: NaiveDateTime,
+    pub updated_at: NaiveDateTime,
+    pub initial_lat: BigDecimal,
+    pub initial_long: BigDecimal,
+    pub final_lat: BigDecimal,
+    pub final_long: BigDecimal,
+    // TODO: Waiting for the driver scope
+    // pub driver_id: Uuid,
+    pub initial_address_id: Option<Uuid>,
+    pub final_address_id: Option<Uuid>,
+    pub vehicle_id: Uuid,
+}
