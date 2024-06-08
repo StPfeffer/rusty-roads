4e77c192f7d574d69fb6e177b6b7d3809f9b0930
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-06-08 19:15:52 -0300

    Driver API routes

--- src/scopes/collaborator.rs
+++ src/scopes/collaborator.rs
@@ -2,10 +2,17 @@ use actix_web::{web, HttpResponse, Scope};
 use validator::Validate;
 
 use crate::{
-    db::collaborator::CollaboratorExt,
+    db::{
+        collaborator::CollaboratorExt,
+        driver::{CnhTypeExt, DriverExt},
+    },
     dtos::{
         collaborator::{
             CollaboratorListResponseDTO, FilterCollaboratorDTO, RegisterCollaboratorDTO,
+        },
+        driver::{
+            CnhTypeListResponseDTO, DriverListResponseDTO, FilterCnhTypeDTO, FilterDriverDTO,
+            RegisterDriverDTO,
         },
         request::RequestQueryDTO,
     },
@@ -16,7 +23,13 @@ use crate::{
 pub fn collaborator_scope() -> Scope {
     web::scope("/api/v1/collaborators")
         .route("", web::get().to(list_collaborators))
+        .route("/drivers", web::post().to(save_driver))
+        .route("/drivers", web::get().to(list_drivers))
+        .route("/drivers/cnh", web::get().to(list_cnh_types))
+        .route("/drivers/cnh/{id}", web::get().to(get_cnh_type))
+        .route("/drivers/{id}", web::get().to(get_driver))
         .route("/{id}", web::get().to(get_collaborator))
+        .route("/{id}/drivers", web::get().to(get_driver_from_collaborator))
         .route("", web::post().to(save_collaborator))
         .route("/{id}", web::delete().to(delete_collaborator))
 }
@@ -31,11 +44,14 @@ pub async fn get_collaborator(
         .await
         .map_err(|e| HttpError::server_error(e.to_string()))?;
 
-    Ok(
-        HttpResponse::Ok().json(FilterCollaboratorDTO::filter_collaborator(
-            &collaborator.unwrap(),
+    match collaborator {
+        Some(collaborator) => {
+            Ok(HttpResponse::Ok().json(FilterCollaboratorDTO::filter_collaborator(&collaborator)))
+        }
+        None => Err(HttpError::from_error_message(
+            ErrorMessage::CollaboratorNotFound,
         )),
-    )
+    }
 }
 
 pub async fn list_collaborators(
@@ -83,7 +99,7 @@ pub async fn save_collaborator(
         Err(sqlx::Error::Database(db_err)) => {
             if db_err.is_unique_violation() {
                 Err(HttpError::unique_constraint_violation(
-                    ErrorMessage::StateExist,
+                    ErrorMessage::CollaboratorExist,
                 ))
             } else {
                 Err(HttpError::server_error(db_err.to_string()))
@@ -97,15 +113,178 @@ pub async fn delete_collaborator(
     id: web::Path<uuid::Uuid>,
     app_state: web::Data<AppState>,
 ) -> Result<HttpResponse, HttpError> {
-    let collaborator = app_state
+    let collaborator_id = Some(id.into_inner());
+
+    app_state
         .db_client
-        .delete_collaborator(Some(id.into_inner()))
+        .delete_driver(None, collaborator_id)
         .await
         .map_err(|e| HttpError::server_error(e.to_string()))?;
 
-    Ok(
-        HttpResponse::Ok().json(FilterCollaboratorDTO::filter_collaborator(
-            &collaborator.unwrap(),
+    let collaborator = app_state
+        .db_client
+        .delete_collaborator(collaborator_id)
+        .await
+        .map_err(|e| HttpError::server_error(e.to_string()))?;
+
+    match collaborator {
+        Some(collaborator) => {
+            Ok(HttpResponse::Ok().json(FilterCollaboratorDTO::filter_collaborator(&collaborator)))
+        }
+        None => Err(HttpError::from_error_message(
+            ErrorMessage::CollaboratorNotFound,
         )),
-    )
+    }
+}
+
+pub async fn get_driver(
+    id: web::Path<uuid::Uuid>,
+    app_state: web::Data<AppState>,
+) -> Result<HttpResponse, HttpError> {
+    let driver = app_state
+        .db_client
+        .get_driver(Some(id.into_inner()), None, None)
+        .await
+        .map_err(|e| HttpError::server_error(e.to_string()))?;
+
+    match driver {
+        Some(driver) => Ok(HttpResponse::Ok().json(FilterDriverDTO::filter_driver(&driver))),
+        None => Err(HttpError::from_error_message(ErrorMessage::DriverNotFound)),
+    }
+}
+
+pub async fn get_driver_from_collaborator(
+    id: web::Path<uuid::Uuid>,
+    app_state: web::Data<AppState>,
+) -> Result<HttpResponse, HttpError> {
+    let driver = app_state
+        .db_client
+        .get_driver(None, None, Some(id.into_inner()))
+        .await
+        .map_err(|e| HttpError::server_error(e.to_string()))?;
+
+    match driver {
+        Some(driver) => Ok(HttpResponse::Ok().json(FilterDriverDTO::filter_driver(&driver))),
+        None => Err(HttpError::from_error_message(ErrorMessage::DriverNotFound)),
+    }
+}
+
+pub async fn list_drivers(
+    query: web::Query<RequestQueryDTO>,
+    app_state: web::Data<AppState>,
+) -> Result<HttpResponse, HttpError> {
+    let query_params: RequestQueryDTO = query.into_inner();
+
+    query_params
+        .validate()
+        .map_err(|e| HttpError::bad_request(e.to_string()))?;
+
+    let page = query_params.page.unwrap_or(1);
+    let limit = query_params.limit.unwrap_or(50);
+
+    let drivers = app_state
+        .db_client
+        .list_drivers(page as u32, limit)
+        .await
+        .map_err(|e| HttpError::server_error(e.to_string()))?;
+
+    Ok(HttpResponse::Ok().json(DriverListResponseDTO {
+        drivers: FilterDriverDTO::filter_drivers(&drivers),
+        results: drivers.len(),
+    }))
+}
+
+pub async fn save_driver(
+    app_state: web::Data<AppState>,
+    body: web::Json<RegisterDriverDTO>,
+) -> Result<HttpResponse, HttpError> {
+    body.validate()
+        .map_err(|e| HttpError::bad_request(e.to_string()))?;
+
+    let result = app_state
+        .db_client
+        .save_driver(
+            &body.cnh_number,
+            body.cnh_expiration_date,
+            &body.id_cnh_type,
+            &body.collaborator_id,
+        )
+        .await;
+
+    match result {
+        Ok(driver) => Ok(HttpResponse::Created().json(FilterDriverDTO::filter_driver(&driver))),
+        Err(sqlx::Error::Database(db_err)) => {
+            if db_err.is_unique_violation() {
+                match db_err.constraint() {
+                    Some(constraint) => {
+                        if constraint == "fk_drivers_collaborator_id"
+                            || constraint == "unq_drivers_cnh_number"
+                        {
+                            Err(HttpError::from_error_message(ErrorMessage::DriverExist))
+                        } else {
+                            Err(HttpError::server_error(db_err.to_string()))
+                        }
+                    }
+                    None => Err(HttpError::server_error(db_err.to_string())),
+                }
+            } else if db_err.is_foreign_key_violation() {
+                match db_err.constraint() {
+                    Some(constraint) => {
+                        if constraint == "fk_drivers_collaborator_id" {
+                            Err(HttpError::from_error_message(
+                                ErrorMessage::CollaboratorNotFound,
+                            ))
+                        } else {
+                            Err(HttpError::from_error_message(ErrorMessage::CnhTypeNotFound))
+                        }
+                    }
+                    None => Err(HttpError::server_error(db_err.to_string())),
+                }
+            } else {
+                Err(HttpError::server_error(db_err.to_string()))
+            }
+        }
+        Err(e) => Err(HttpError::server_error(e.to_string())),
+    }
+}
+
+pub async fn get_cnh_type(
+    id: web::Path<uuid::Uuid>,
+    app_state: web::Data<AppState>,
+) -> Result<HttpResponse, HttpError> {
+    let cnh_type = app_state
+        .db_client
+        .get_cnh_type(Some(id.into_inner()), None)
+        .await
+        .map_err(|e| HttpError::server_error(e.to_string()))?;
+
+    match cnh_type {
+        Some(cnh_type) => Ok(HttpResponse::Ok().json(FilterCnhTypeDTO::filter_cnh_type(&cnh_type))),
+        None => Err(HttpError::from_error_message(ErrorMessage::DriverNotFound)),
+    }
+}
+
+pub async fn list_cnh_types(
+    query: web::Query<RequestQueryDTO>,
+    app_state: web::Data<AppState>,
+) -> Result<HttpResponse, HttpError> {
+    let query_params: RequestQueryDTO = query.into_inner();
+
+    query_params
+        .validate()
+        .map_err(|e| HttpError::bad_request(e.to_string()))?;
+
+    let page = query_params.page.unwrap_or(1);
+    let limit = query_params.limit.unwrap_or(50);
+
+    let cnh_types = app_state
+        .db_client
+        .list_cnh_type(page as u32, limit)
+        .await
+        .map_err(|e| HttpError::server_error(e.to_string()))?;
+
+    Ok(HttpResponse::Ok().json(CnhTypeListResponseDTO {
+        types: FilterCnhTypeDTO::filter_cnh_types(&cnh_types),
+        results: cnh_types.len(),
+    }))
 }
