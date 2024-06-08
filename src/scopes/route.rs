3cd1b818e1594d88654a60ae7b30be6115d2e212
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-06-07 23:43:38 -0300

    Added more RouteStatus API endpoints

--- src/scopes/route.rs
+++ src/scopes/route.rs
@@ -5,7 +5,10 @@ use crate::{
     db::route::{RouteExt, RouteStatusExt},
     dtos::{
         request::RequestQueryDTO,
-        route::{FilterRouteDTO, FilterRouteStatusDTO, RegisterRouteDTO, RouteListResponseDTO},
+        route::{
+            FilterRouteDTO, FilterRouteStatusDTO, RegisterRouteDTO, RegisterRouteStatusDTO,
+            RouteListResponseDTO, RouteStatusListResponseDTO,
+        },
     },
     error::{ErrorMessage, HttpError},
     AppState,
@@ -14,8 +17,11 @@ use crate::{
 pub fn route_scope() -> Scope {
     web::scope("/api/v1/routes")
         .route("", web::get().to(list_routes))
-        .route("/{id}", web::get().to(get_route))
         .route("", web::post().to(save_route))
+        .route("/status", web::post().to(save_route_status))
+        .route("/status/{id}", web::delete().to(delete_route_status))
+        .route("/status", web::get().to(list_route_status))
+        .route("/{id}", web::get().to(get_route))
         .route("/{id}", web::delete().to(delete_route))
         .route("/{id}/status", web::get().to(get_route_status))
 }
@@ -110,7 +116,6 @@ pub async fn delete_route(
 
 pub async fn get_route_status(
     id: web::Path<uuid::Uuid>,
-    code: Option<String>,
     app_state: web::Data<AppState>,
 ) -> Result<HttpResponse, HttpError> {
     let route = app_state
@@ -124,7 +129,11 @@ pub async fn get_route_status(
         Some(route) => {
             status_id = route.status_id;
         }
-        None => return Err(HttpError::from_error_message(ErrorMessage::RouteStatusNotFound)),
+        None => {
+            return Err(HttpError::from_error_message(
+                ErrorMessage::RouteStatusNotFound,
+            ))
+        }
     }
 
     let status = app_state
@@ -137,6 +146,82 @@ pub async fn get_route_status(
         Some(status) => {
             Ok(HttpResponse::Ok().json(FilterRouteStatusDTO::filter_route_status(&status)))
         }
-        None => Err(HttpError::from_error_message(ErrorMessage::RouteStatusNotFound)),
+        None => Err(HttpError::from_error_message(
+            ErrorMessage::RouteStatusNotFound,
+        )),
+    }
+}
+
+pub async fn list_route_status(
+    query: web::Query<RequestQueryDTO>,
+    app_state: web::Data<AppState>,
+) -> Result<HttpResponse, HttpError> {
+    let query_params: RequestQueryDTO = query.into_inner();
+
+    query_params
+        .validate()
+        .map_err(|e| HttpError::bad_request(e.to_string()))?;
+
+    let page = query_params.page.unwrap_or(1);
+    let limit = query_params.limit.unwrap_or(50);
+
+    let statuses = app_state
+        .db_client
+        .list_route_status(page as u32, limit)
+        .await
+        .map_err(|e| HttpError::server_error(e.to_string()))?;
+
+    Ok(HttpResponse::Ok().json(RouteStatusListResponseDTO {
+        status: FilterRouteStatusDTO::filter_route_statuses(&statuses),
+        results: statuses.len(),
+    }))
+}
+
+pub async fn save_route_status(
+    app_state: web::Data<AppState>,
+    body: web::Json<RegisterRouteStatusDTO>,
+) -> Result<HttpResponse, HttpError> {
+    body.validate()
+        .map_err(|e| HttpError::bad_request(e.to_string()))?;
+
+    let result = app_state
+        .db_client
+        .save_route_status(body.code.as_ref(), &body.description)
+        .await;
+
+    match result {
+        Ok(status) => {
+            Ok(HttpResponse::Created().json(FilterRouteStatusDTO::filter_route_status(&status)))
+        }
+        Err(sqlx::Error::Database(db_err)) => {
+            if db_err.is_unique_violation() {
+                Err(HttpError::unique_constraint_violation(
+                    ErrorMessage::AddressExist,
+                ))
+            } else if db_err.is_foreign_key_violation() {
+                Err(HttpError::bad_request(ErrorMessage::CityNotFound))
+            } else {
+                Err(HttpError::server_error(db_err.to_string()))
+            }
+        }
+        Err(e) => Err(HttpError::server_error(e.to_string())),
+    }
+}
+
+pub async fn delete_route_status(
+    id: web::Path<uuid::Uuid>,
+    app_state: web::Data<AppState>,
+) -> Result<HttpResponse, HttpError> {
+    let status = app_state
+        .db_client
+        .delete_route_status(Some(id.into_inner()))
+        .await
+        .map_err(|e| HttpError::server_error(e.to_string()))?;
+
+    match status {
+        Some(status) => {
+            Ok(HttpResponse::Ok().json(FilterRouteStatusDTO::filter_route_status(&status)))
+        }
+        None => Err(HttpError::from_error_message(ErrorMessage::RouteNotFound)),
     }
 }
