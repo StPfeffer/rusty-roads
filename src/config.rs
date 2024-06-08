7a985d58c9be9646cc6ffe55679729f3ccb27167
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-06-08 15:16:49 -0300

    Validation to load the correct DATABASE_URL based on where the application is running

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,5 @@
+use in_container::in_container;
+
 /// Represents the configuration settings for the application.
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -33,7 +35,15 @@ impl Config {
     /// println!("{:?}", config);
     /// ```
     pub fn init() -> Config {
-        let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+        let mut database_url;
+
+        if (in_container()) {
+            database_url =
+                std::env::var("DATABASE_URL_DOCKER").expect("DATABASE_URL_DOCKER must be set");
+        } else {
+            database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+        }
+
         let jwt_secret = std::env::var("JWT_SECRET_KEY").expect("JWT_SECRET_KEY must be set");
         let jwt_maxage = std::env::var("JWT_MAXAGE").expect("JWT_MAXAGE must be set");
 
